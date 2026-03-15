ad34e5e97311b33aad286c59d949016995407bf6
VivekShukla-2504 <7800652515vivek@gmail.com>
2026-03-15 11:11:48 +0530

    Slighty Change

--- Array/linear.java
+++ Array/linear.java
@@ -3,36 +3,39 @@ package Array;
 import java.util.Scanner;
 
 public class linear {
+
     public static void main(String[] args) {
-       int[] nums = { 2,8,40,37,-14,28,-42};
-       System.out.println("Enter the Element which you want to find");
-       Scanner sc = new Scanner(System.in);
-       int target = sc.nextInt();
-       int ans = linearSearch(nums, target);
-       if(ans!=-1)
-       {
-       System.out.println(target + " at index "+ans);
-       }
-       else 
-       {
-        System.out.println( target +" Not found ");
-       }
+
+        int[] nums = {2,8,40,37,-14,28,-42};
+
+        System.out.println("Enter the Element which you want to find");
+
+        Scanner sc = new Scanner(System.in);
+        int target = sc.nextInt();
+
+        int ans = linearSearch(nums, target);
+
+        if(ans != -1){
+            System.out.println(target + " at index " + ans);
+        }else{
+            System.out.println(target + " Not found");
+        }
+
+        sc.close();
     }
 
-static int linearSearch(int [] arr,int target)
-{
- if(arr.length==0)
- {
-    return -1;
- }
- for(int i =0;i<arr.length;i++)
- {
-    int element = arr[i];
-    if(element==target)
-    {
-        return i;
+    static int linearSearch(int[] arr, int target){
+
+        if(arr.length == 0){
+            return -1;
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == target){
+                return i;
+            }
+        }
+
+        return -1;
     }
- }
- return -1;
-}
 }
